f0e413970b20075a64d4e8018ae1b05f170207da
Ardelean Adrian <ardelean.adrian.mihai@gmail.com>
2022-03-04 20:31:02 +0200

    Destroy old room data values

--- app/services/process_room_sensors_file.rb
+++ app/services/process_room_sensors_file.rb
@@ -5,11 +5,8 @@ class ProcessRoomSensorsFile
 
   def perform
     @room_sensors_file.update!(status: PersonalFile::PROCESSING)
-
-    user.temperature_values.create(Extract::RoomSensors::Temperature.new(uuid: @room_sensors_file.uuid).perform)
-    user.humidity_values.create(Extract::RoomSensors::Humidity.new(uuid: @room_sensors_file.uuid).perform)
-    user.co2_values.create(Extract::RoomSensors::Co2.new(uuid: @room_sensors_file.uuid).perform)
-
+    destroy_old_values
+    create_new_values
     @room_sensors_file.update!(status: PersonalFile::PROCESSED)
 
     self
@@ -21,5 +18,17 @@ class ProcessRoomSensorsFile
     return @user if defined? @user
 
     @user = @room_sensors_file.user
+  end
+
+  def destroy_old_values
+    user.temperature_values.destroy_all
+    user.humidity_values.destroy_all
+    user.co2_values.destroy_all
+  end
+
+  def create_new_values
+    user.temperature_values.create(Extract::RoomSensors::Temperature.new(uuid: @room_sensors_file.uuid).perform)
+    user.humidity_values.create(Extract::RoomSensors::Humidity.new(uuid: @room_sensors_file.uuid).perform)
+    user.co2_values.create(Extract::RoomSensors::Co2.new(uuid: @room_sensors_file.uuid).perform)
   end
 end
--- app/services/process_samsung_health_file.rb
+++ app/services/process_samsung_health_file.rb
@@ -5,12 +5,7 @@ class ProcessSamsungHealthFile
 
   def perform
     @samsung_health_file.update!(status: PersonalFile::PROCESSING)
-
-    user.sleep_sessions.create(Extract::Sleep.new(uuid: @samsung_health_file.uuid).perform)
-    user.heart_rate_values.create(Extract::HeartRate.new(uuid: @samsung_health_file.uuid).perform)
-    user.stress_values.create(Extract::Stress.new(uuid: @samsung_health_file.uuid).perform)
-    user.exercises.create(Extract::Exercises.new(uuid: @samsung_health_file.uuid).perform)
-
+    create_new_values
     @samsung_health_file.update!(status: PersonalFile::PROCESSED)
 
     self
@@ -22,5 +17,12 @@ class ProcessSamsungHealthFile
     return @user if defined? @user
 
     @user = @samsung_health_file.user
+  end
+
+  def create_new_values
+    user.sleep_sessions.create(Extract::Sleep.new(uuid: @samsung_health_file.uuid).perform)
+    user.heart_rate_values.create(Extract::HeartRate.new(uuid: @samsung_health_file.uuid).perform)
+    user.stress_values.create(Extract::Stress.new(uuid: @samsung_health_file.uuid).perform)
+    user.exercises.create(Extract::Exercises.new(uuid: @samsung_health_file.uuid).perform)
   end
 end
